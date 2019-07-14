2d2a55eddb933f9d9c7a609c8b4cd3ba55afe734
zhao <ex-zhaoweizhi001@pingan.com.cn>
2019-07-14 16:04:16 +0800

    1.创建空工程

---
+++ OCTemplateDemo/OCTemplateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCTemplateDemo
+//
+//  Created by Ethon on 2019/7/14.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCTemplateDemo/OCTemplateDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OCTemplateDemo
+//
+//  Created by Ethon on 2019/7/14.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

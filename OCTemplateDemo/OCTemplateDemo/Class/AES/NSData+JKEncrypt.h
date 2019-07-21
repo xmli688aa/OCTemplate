552ad8085429acf50dec5c184c6ac6319509e87e
zhao <xmli688aa@gmail.com>
2019-07-21 21:37:27 +0800

    AES加密相关

---
+++ OCTemplateDemo/OCTemplateDemo/Class/AES/NSData+JKEncrypt.h
@@ -0,0 +1,80 @@
+//
+//  NSData+JKEncrypt.h
+//  JKCategories (https://github.com/shaojiankui/JKCategories)
+//
+//  Created by Jakey on 14/12/15.
+//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (JKEncrypt)
+
+/**
+ *  利用AES加密数据
+ *
+ *  @param key key
+ *  @param iv  iv description
+ *
+ *  @return data
+ */
+- (NSData *)jk_encryptedWithAESUsingKey:(NSString*)key andIV:(NSData*)iv;
+/**
+ *  @brief  利用AES解密据
+ *
+ *  @param key key
+ *  @param iv  iv
+ *
+ *  @return 解密后数据
+ */
+- (NSData *)jk_decryptedWithAESUsingKey:(NSString*)key andIV:(NSData*)iv;
+
+/**
+ *  利用3DES加密数据
+ *
+ *  @param key key
+ *  @param iv  iv description
+ *
+ *  @return data
+ */
+- (NSData *)jk_encryptedWith3DESUsingKey:(NSString*)key andIV:(NSData*)iv;
+/**
+ *  @brief   利用3DES解密数据
+ *
+ *  @param key key
+ *  @param iv  iv
+ *
+ *  @return 解密后数据
+ */
+- (NSData *)jk_decryptedWith3DESUsingKey:(NSString*)key andIV:(NSData*)iv;
+
+/**
+ *  @brief  NSData 转成UTF8 字符串
+ *
+ *  @return 转成UTF8 字符串
+ */
+- (NSString *)jk_UTF8String;
+
+
+/**
+ aes加密 key为字符串
+ */
+- (NSData *)aes256_encrypt:(NSString *)key;
+
+/**
+ aes解密 key为字符串
+ */
+- (NSData *)aes256_decrypt:(NSString *)key;
+
+/**
+ aes加密 key为Data
+ */
+- (NSData *)aes256_encryptData:(NSData *)keyData;
+
+/**
+ aes解密 key为Data
+ */
+- (NSData *)aes256_decryptData:(NSData *)keyData;
+
+
+@end

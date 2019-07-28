36ef48c90981bc6cb4a711eedf21fa8193f7b97a
zhao <xmli688aa@gmail.com>
2019-07-28 15:04:28 +0800

    mvvm

---
+++ OCTemplateDemo/OCTemplateDemo/Class/Test/MVVM/ETNews.h
@@ -0,0 +1,20 @@
+//
+//  ETNews.h
+//  OCTemplateDemo
+//
+//  Created by Ethon on 2019/7/28.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import "ETBaseModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ETNews : ETBaseModel
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *content;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCTemplateDemo/OCTemplateDemo/Class/Test/MVVM/ETNewsVM.m
@@ -0,0 +1,30 @@
+//
+//  ETNewsVM.m
+//  OCTemplateDemo
+//
+//  Created by Ethon on 2019/7/28.
+//  Copyright © 2019 zhao. All rights reserved.
+//
+
+#import "ETNewsVM.h"
+#import "ETNews.h"
+
+@implementation ETNewsVM
+
+- (void)loadNewsData:(void(^)(NSArray *))completion{
+    if (completion == nil) {
+        return;
+    }
+    //这里网络请求数据 并对返回的数据进行处理 包装成对象返回
+    NSMutableArray *newsData = [NSMutableArray array];
+    for (int i = 0; i<10; i++) {
+        ETNews *news = [[ETNews alloc] init];
+        news.title = [NSString stringWithFormat:@"我是t标题:%d",i];
+        news.content = [NSString stringWithFormat:@"我是内容:%d",i];
+        [newsData addObject:news];
+    }
+    completion(newsData);
+    
+}
+
+@end

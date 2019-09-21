6721147c574fb6eef82581d446c3fe286dc1d229
Ethon.Z <>
2019-09-21 13:52:33 +0800

    图片轮播

--- OCTemplateDemo/OCTemplateDemo/Class/控件封装/图片轮播/ETInfiniteScrollView.h
+++ OCTemplateDemo/OCTemplateDemo/Class/控件封装/图片轮播/ETInfiniteScrollView.h
@@ -1,22 +1,22 @@
 //
-//  XMGInfiniteScrollView.h
-//  06-03-分页
+//  ETInfiniteScrollView.h
+//  OCTemplateDemo
 //
-//  Created by MJ Lee on 15/5/24.
-//  Copyright (c) 2015年 小码哥. All rights reserved.
+//  Created by Ethon.Z on 2019/9/21.
+//  Copyright (c) Hello  All rights reserves.
 //
 
 #import <UIKit/UIKit.h>
 
-@protocol XMGInfiniteScrollViewDelegate <NSObject>
+@protocol ETInfiniteScrollViewDelegate <NSObject>
 
 - (void)clickImageViewWithIndex:(NSInteger )index;
 
 @end
 
-typedef void(^XMGInfiniteScrollViewBlock)(NSInteger clickIndex);
+typedef void(^ETInfiniteScrollViewBlock)(NSInteger clickIndex);
 
-@interface XMGInfiniteScrollView : UIView
+@interface ETInfiniteScrollView : UIView
 @property (strong, nonatomic) NSArray *images;
 @property (weak, nonatomic, readonly) UIPageControl *pageControl;
 @property (assign, nonatomic, getter=isScrollDirectionPortrait) BOOL scrollDirectionPortrait;//yes表示竖直滚动 No表示水平滚动
@@ -25,7 +25,7 @@ typedef void(^XMGInfiniteScrollViewBlock)(NSInteger clickIndex);
 //点击图片触发操作可以通过block或者代理回传
 
 //用blcok传值
-@property (nonatomic, copy) XMGInfiniteScrollViewBlock block;
+@property (nonatomic, copy) ETInfiniteScrollViewBlock block;
 //用代理传值
-@property (nonatomic, weak) id<XMGInfiniteScrollViewDelegate> delegate;
+@property (nonatomic, weak) id<ETInfiniteScrollViewDelegate> delegate;
 @end
--- OCTemplateDemo/OCTemplateDemo/Class/控件封装/图片轮播/ETInfiniteScrollView.m
+++ OCTemplateDemo/OCTemplateDemo/Class/控件封装/图片轮播/ETInfiniteScrollView.m
@@ -1,22 +1,22 @@
 //
-//  XMGInfiniteScrollView.m
-//  06-03-分页
+//  ETInfiniteScrollView.m
+//  OCTemplateDemo
 //
-//  Created by MJ Lee on 15/5/24.
-//  Copyright (c) 2015年 小码哥. All rights reserved.
+//  Created by Ethon.Z on 2019/9/21.
+//  Copyright (c) Hello  All rights reserves.
 //
 
-#import "XMGInfiniteScrollView.h"
+#import "ETInfiniteScrollView.h"
 
 static int const ImageViewCount = 3;
 
-@interface XMGInfiniteScrollView() <UIScrollViewDelegate,UIGestureRecognizerDelegate>
+@interface ETInfiniteScrollView() <UIScrollViewDelegate,UIGestureRecognizerDelegate>
 @property (weak, nonatomic) UIScrollView *scrollView;
 @property (weak, nonatomic) NSTimer *timer;
 @property (nonatomic, assign) NSInteger clickIndex;
 @end
 
-@implementation XMGInfiniteScrollView
+@implementation ETInfiniteScrollView
 
 - (instancetype)initWithFrame:(CGRect)frame
 {

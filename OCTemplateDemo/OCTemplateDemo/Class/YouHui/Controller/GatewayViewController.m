5d48149896ac24bd7c016ade9f9103ec6de0c1c5
zhao <xmli688aa@gmail.com>
2019-10-13 11:41:22 +0800

    删除废弃文件

--- OCTemplateDemo/OCTemplateDemo/Class/YouHui/Controller/GatewayViewController.m
+++ OCTemplateDemo/OCTemplateDemo/Class/YouHui/Controller/GatewayViewController.m
@@ -7,59 +7,28 @@
 //
 
 #import "GatewayViewController.h"
-#import "ETSearchContentView.h"
-#import "ETSearchTitleView.h"
-#import "ETSearchViewController.h"
 
-@interface GatewayViewController ()<ETSearchTitleViewDelegate>
-@property (nonatomic, strong) ETSearchTitleView *titleView;
-@property (nonatomic, strong) ETSearchContentView *searchContentView;
+
+@interface GatewayViewController ()
+
+
 @end
 
 @implementation GatewayViewController
 
-- (ETSearchContentView *)searchContentView{
-    if (_searchContentView == nil) {
-        _searchContentView = [[ETSearchContentView alloc] initWithFrame:CGRectMake(0, kStatusBarHeight+50, kScreenWidth, kScreenHeight - kStatusBarHeight -50)];
-    }
-    return _searchContentView;
-}
-- (ETSearchTitleView *)titleView{
-    if (_titleView == nil) {
-        _titleView = [[ETSearchTitleView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kStatusBarHeight +50)];
-        _titleView.deleagete = self;
-        _titleView.funType = RightBtnType_Publish;
-    }
-    return _titleView;
-}
+
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    self.navigationController.navigationBarHidden = YES;
 }
 - (void)viewDidDisappear:(BOOL)animated{
     [super viewDidDisappear:animated];
-    self.navigationController.navigationBarHidden = NO;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.view addSubview:self.titleView];
-    self.view.backgroundColor = [UIColor redColor];
 
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    [self.view endEditing:YES];
-}
-#pragma ETSearchTitleViewDelegate
-- (void)textFieldDidBeginEditing{
-    NSLog(@"开始了");
-//    [self.view addSubview:self.searchContentView];
-    [self.navigationController pushViewController:[ETSearchViewController new] animated:NO];
 
-}
-- (void)textFieldInputString:(NSString *)inputString{
-    NSLog(@"输出的字符串:%@",inputString);
-    [self.searchContentView updateSearchContent:inputString];    
-}
+
 
 @end

ae979da587d98760480e17821d1c132accc3d38b
zwz <xmli688aa@gmail.com>
2023-03-25 20:40:36 +0800

    【控制器无法左上角无法返回】

--- OCTemplateDemo/OCTemplateDemo/Class/Study/上下拉设置导航栏颜色透明/ETPersonDetailVCViewController.h
+++ OCTemplateDemo/OCTemplateDemo/Class/Study/上下拉设置导航栏颜色透明/ETPersonDetailVCViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ETPersonDetailVCViewController : UIViewController
+@interface ETPersonDetailVCViewController : ETBaseViewController
 
 @end
 
--- OCTemplateDemo/OCTemplateDemo/Class/Study/上下拉设置导航栏颜色透明/ETPersonDetailVCViewController.m
+++ OCTemplateDemo/OCTemplateDemo/Class/Study/上下拉设置导航栏颜色透明/ETPersonDetailVCViewController.m
@@ -133,7 +133,7 @@
     
 }
 - (void)back{
-    [self.navigationController popoverPresentationController];
+    [self.navigationController popViewControllerAnimated:YES];
 }
 
 
